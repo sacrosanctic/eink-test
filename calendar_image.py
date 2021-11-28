14f75098e46c70f924f79ea1ad5f5926abdb24d6
Scott Wu <sw@scottwu.ca>
2021-11-28 11:30:32 +0800

    1

--- calendar_image.py
+++ calendar_image.py
@@ -29,7 +29,7 @@ try:
     #epd.Clear()
 
     logging.info("3.read bmp file")
-    Himage = Image.open(os.path.join(imgdir, 'calendar2.png'))
+    Himage = Image.open(os.path.join(imgdir, 'calendar800.png'))
     Himage = Himage.resize((epd.height, epd.width))
     Himage_Other = Image.new('1', (epd.height, epd.width), 255)
     epd.display(epd.getbuffer(Himage),epd.getbuffer(Himage_Other))
