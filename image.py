ee5c49328c69d52d038e1bb4225b0d1d2b2fa561
Scott Wu <sw@scottwu.ca>
2021-11-28 10:14:22 +0800

    1

---
+++ image.py
@@ -0,0 +1,50 @@
+
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from lib import epd7in5b_V2
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+
+import requests
+import shutil
+from datetime import date
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:
+    logging.info("epd7in5b_V2 Demo")
+
+    epd = epd7in5b_V2.EPD()
+    logging.info("init and Clear")
+    epd.init()
+    #epd.Clear()
+
+    logging.info("3.read bmp file")
+    Himage = Image.open(os.path.join(picdir, '7in5_V2_b.bmp'))
+    Himage_Other = Image.open(os.path.join(picdir, '7in5_V2_r.bmp'))
+    epd.display(epd.getbuffer(Himage),epd.getbuffer(Himage_Other))
+    time.sleep(30)
+
+    logging.info("Clear...")
+    #epd.init()
+    #epd.Clear()
+
+    logging.info("Goto Sleep...")
+    epd.sleep()
+    
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    epd7in5b_V2.epdconfig.module_exit()
+    exit()
